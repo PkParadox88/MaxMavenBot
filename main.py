ca2d8f152ed585ef1998c72b2f58f7ba48711780
PRAHLAD KUMAR <prahladkumar3741@gmail.com>
2023-09-19 16:45:44 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,42 @@
-import asyncio
-import logging
-import sys
-from os import getenv
+import requests
+import json
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+# Replace with your Telegram bot token
+BOT_TOKEN = "6526048633:AAHWo8UD4h7R64LV_R7U4YcXg-fU_7WiHLY"
 
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv("BOT_TOKEN", "6526048633:AAHWo8UD4h7R64LV_R7U4YcXg-fU_7WiHLY")
-
-# All handlers should be attached to the Router (or Dispatcher)
-dp = Dispatcher()
+# Define the base URL for the Telegram Bot API
+BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+def get_updates(offset=None):
+    url = f"{BASE_URL}/getUpdates"
+    params = {"offset": offset, "timeout": 60}
+    response = requests.get(url, params=params)
+    return response.json()
 
 
-# Echo handler
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+def send_message(chat_id, text):
+    url = f"{BASE_URL}/sendMessage"
+    data = {"chat_id": chat_id, "text": text}
+    response = requests.post(url, data=data)
+    return response.json()
 
 
-async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
-    await dp.start_polling(bot)
+def main():
+    offset = None
+    while True:
+        updates = get_updates(offset)
+        for update in updates.get("result", []):
+            message = update.get("message")
+            chat_id = message.get("chat").get("id")
+            text = message.get("text")
+
+            if text:
+                send_message(chat_id, text)
+
+            # Set the new offset to avoid processing the same update again
+            offset = update.get("update_id") + 1
 
 
-logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-asyncio.run(main())
+if __name__ == "__main__":
+    main()
